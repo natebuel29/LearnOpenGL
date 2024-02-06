0efbf7ad73ecdfe113123a295d7b1794c0d85796
Nate Buel <natebuel29@gmail.com>
2024-02-05 19:28:58 -0600

    setup assimp

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -4,7 +4,7 @@
 #include "Shader.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-
+#include <assimp\scene.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
